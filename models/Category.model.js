95e5dabedefeb2292c73a41b4858e49362737eb7
Henry Ebose Adedugba <henryadedugba@gmail.com>
2024-09-24 00:43:48 +0100

    Update_two

--- models/Category.model.js
+++ models/Category.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 // const { use } = require('../routes/user.route');
 
 //Define Schema
